71195356f2f14447a78d90a84ce16788332d0c36
Fantix King <fantix.king@gmail.com>
2025-07-27 16:26:28 +0200

    Drop the `--` separator in `gel generate` (#1698)
    Fixes #1695

--- src/commands/generate.rs
+++ src/commands/generate.rs
@@ -11,12 +11,55 @@ use which::which;
 use crate::commands::options::Options;
 
 #[derive(clap::Args, Debug, Clone)]
+#[command(disable_help_flag = true)]
 pub struct Command {
-    /// The generator to use, in the form `<lang>/<tool>`. Currently available: py/queries,
-    /// py/models, js/edgeql-js, js/queries, js/interfaces, js/prisma.
-    pub generator: String,
+    /// Print help (see more with '--help')
+    #[arg(short = 'h')]
+    short_help: bool,
+
+    /// Print help (see a summary with '-h')
+    #[arg(long = "help")]
+    long_help: bool,
+
     /// Arguments to pass to the generator.
+    #[arg(trailing_var_arg = true)]
     pub arguments: Vec<String>,
+}
+
+fn print_help(short_help: bool) -> anyhow::Result<()> {
+    let mut app = crate::options::Options::command().mut_subcommand("generate", |cmd| {
+        cmd.arg(
+            clap::arg!(<GENERATOR>)
+                .value_parser([
+                    "py/queries",
+                    "py/models",
+                    "js/edgeql-js",
+                    "js/queries",
+                    "js/interfaces",
+                    "js/prisma",
+                ])
+                .help(color_print::cstr!(
+                    "The generator to use, in the form <bold><<lang>>/<<tool>></bold>."
+                ))
+                .index(1),
+        )
+        .mut_args(|arg| match arg.get_id().as_str() {
+            "arguments" => arg.index(2),
+            "short_help" if short_help => arg.long("help"),
+            "long_help" if !short_help => arg.short('h'),
+            "short_help" | "long_help" => arg.short(None).long("hide").hide(true),
+            _ => arg,
+        })
+    });
+    let generate = app
+        .find_subcommand_mut("generate")
+        .expect("generate subcommand should exist");
+    if short_help {
+        generate.print_help()?;
+    } else {
+        generate.print_long_help()?;
+    }
+    Ok(())
 }
 
 /// How we should invoke via `uv run` (or not).
@@ -44,7 +87,9 @@ impl UseUv {
 
 pub fn prepare_command(cmd: &Command) -> Result<Vec<OsString>, anyhow::Error> {
     let (lang, generator) = cmd
-        .generator
+        .arguments
+        .first()
+        .expect("generator argument is required")
         .split_once('/')
         .context("generator should be of form <lang>/<tool>")?;
 
@@ -97,6 +142,10 @@ pub fn prepare_command(cmd: &Command) -> Result<Vec<OsString>, anyhow::Error> {
 }
 
 pub async fn run(cmd: &Command, options: &Options) -> Result<(), anyhow::Error> {
+    if cmd.short_help || cmd.long_help || cmd.arguments.is_empty() {
+        return print_help(cmd.short_help || !cmd.long_help);
+    }
+
     let creds = options.conn_params.get()?.as_credentials()?;
     let json = serde_json::to_string(&creds)?;
     let mut cred_file = NamedTempFile::new()?;
@@ -105,7 +154,7 @@ pub async fn run(cmd: &Command, options: &Options) -> Result<(), anyhow::Error> 
     let cmdline = prepare_command(cmd)?;
     let mut scmd = process::Command::new(cmdline[0].clone());
     scmd.args(&cmdline[1..])
-        .args(cmd.arguments.clone())
+        .args(cmd.arguments.iter().skip(1).map(|s| s.clone()))
         .stdin(Stdio::null())
         .stdout(Stdio::inherit());
     // Strip out all gel config env vars from the environment, since
