geldata/gel-cli

commit 19faba1b24e14b26e3e45339ee1b52ca2718a613

Fix extension install (#1752)
More nested tokio runtime fixes. Follow up to #1747.

src/cli/upgrade.rs
@@ -49,7 +49,8 @@ pub fn run(cmd: &Command) -> anyhow::Result<()> {
     upgrade(cmd, _get_upgrade_path()?)
 }
 
-fn upgrade(cmd: &Command, path: PathBuf) -> anyhow::Result<()> {
+#[tokio::main(flavor = "current_thread")]
+async fn upgrade(cmd: &Command, path: PathBuf) -> anyhow::Result<()> {
     let cur_channel = channel();
     let channel = if let Some(channel) = cmd.to_channel {
         channel
@@ -95,7 +96,7 @@ fn upgrade(cmd: &Command, path: PathBuf) -> anyhow::Result<()> {
     let down_path = path.with_extension("download");
     let tmp_path = tmp_file_path(&path);
 
-    download(&down_path, &pkg.url, cmd.quiet)?;
+    download(&down_path, &pkg.url, cmd.quiet).await?;
     unpack_file(&down_path, &tmp_path, pkg.compression)?;
 
     let backup_path = path.with_extension("backup");

src/migrations/prompt.rs
@@ -70,7 +70,7 @@ pub fn expression(
     use std::io::Write;
 
     if !std::io::stdin().is_terminal() {
-        print!("{}", prompt);
+        print!("{prompt}");
         _ = std::io::stdout().flush();
         let mut buf = String::new();
         std::io::stdin().read_line(&mut buf)?;

src/portable/extension.rs
@@ -197,7 +197,7 @@ async fn install(cmd: &ExtensionInstall, _options: &Options) -> Result<(), anyho
                 cmd.extension,
                 pkg.version.to_string().emphasized()
             );
-            let zip = download_package(pkg)?;
+            let zip = download_package(pkg).await?;
             let command = if cmd.reinstall {
                 Some("--reinstall")
             } else {

src/portable/repository.rs
@@ -464,8 +464,16 @@ pub async fn get_specific_package(version: &ver::Specific) -> anyhow::Result<Opt
     Ok(pkg)
 }
 
-#[context("failed to download file at URL: {}", url)]
 #[tokio::main(flavor = "current_thread")]
+pub async fn download_sync(
+    dest: impl AsRef<Path>,
+    url: &Url,
+    quiet: bool,
+) -> Result<blake2b_simd::Hash, anyhow::Error> {
+    download(dest, url, quiet).await
+}
+
+#[context("failed to download file at URL: {}", url)]
 pub async fn download(
     dest: impl AsRef<Path>,
     url: &Url,

src/portable/server/install.rs
@@ -76,7 +76,8 @@ pub fn specific(version: &ver::Specific) -> anyhow::Result<InstallInfo> {
     package(&pkg)
 }
 
-pub fn package(pkg_info: &PackageInfo) -> anyhow::Result<InstallInfo> {
+#[tokio::main(flavor = "current_thread")]
+pub async fn package(pkg_info: &PackageInfo) -> anyhow::Result<InstallInfo> {
     let ver_name = pkg_info.version.specific().to_string();
     let target_dir = platform::portable_dir()?.join(ver_name);
     if target_dir.exists() {
@@ -95,7 +96,7 @@ pub fn package(pkg_info: &PackageInfo) -> anyhow::Result<InstallInfo> {
     }
 
     print::msg!("Downloading package...");
-    let cache_path = download_package(pkg_info)?;
+    let cache_path = download_package(pkg_info).await?;
     let tmp_target = platform::tmp_file_path(&target_dir);
     unpack_package(&cache_path, &tmp_target)?;
     let info = InstallInfo {
@@ -142,12 +143,12 @@ fn check_metadata(dir: &Path, pkg_info: &PackageInfo) -> anyhow::Result<InstallI
 }
 
 #[context("failed to download {}", pkg_info)]
-pub fn download_package(pkg_info: &PackageInfo) -> anyhow::Result<PathBuf> {
+pub async fn download_package(pkg_info: &PackageInfo) -> anyhow::Result<PathBuf> {
     let cache_dir = platform::cache_dir()?;
     let download_dir = cache_dir.join("downloads");
     fs::create_dir_all(&download_dir)?;
     let cache_path = download_dir.join(pkg_info.cache_file_name());
-    let hash = download(&cache_path, &pkg_info.url, false)?;
+    let hash = download(&cache_path, &pkg_info.url, false).await?;
     match &pkg_info.hash {
         PackageHash::Blake2b(hex) => {
             if hash.to_hex()[..] != hex[..] {

src/portable/windows.rs
@@ -37,7 +37,7 @@ use crate::platform::{cache_dir, config_dir, tmp_file_path, wsl_dir};
 use crate::portable::exit_codes;
 use crate::portable::local::{InstanceInfo, NonLocalInstance, Paths, write_json};
 use crate::portable::options;
-use crate::portable::repository::{self, PackageHash, PackageInfo, download};
+use crate::portable::repository::{self, PackageHash, PackageInfo, download, download_sync};
 use crate::portable::server;
 use crate::portable::ver;
 use crate::print::{self, Highlight, msg};
@@ -410,7 +410,8 @@ fn wsl_cli_version(distro: &str) -> anyhow::Result<ver::Semver> {
 }
 
 #[cfg(windows)]
-fn download_binary(dest: &Path) -> anyhow::Result<()> {
+#[tokio::main(flavor = "current_thread")]
+async fn download_binary(dest: &Path) -> anyhow::Result<()> {
     let my_ver = self_version()?;
     let (arch, _) = crate::portable::platform::get_cli()?
         .split_once('-')
@@ -452,7 +453,7 @@ fn download_binary(dest: &Path) -> anyhow::Result<()> {
 
     let down_path = dest.with_extension("download");
     let tmp_path = tmp_file_path(dest);
-    download(&down_path, &pkg.url, false)?;
+    download(&down_path, &pkg.url, false).await?;
     upgrade::unpack_file(&down_path, &tmp_path, pkg.compression)?;
     fs_err::rename(&tmp_path, dest)?;
 
@@ -491,6 +492,8 @@ fn get_wsl_lib() -> anyhow::Result<&'static wslapi::Library> {
 
 #[cfg(windows)]
 #[context("cannot initialize WSL2 (windows subsystem for linux)")]
+// N.B (asdf): can't call with install=True if there is a running tokio runtime
+// asdf!
 fn get_wsl_distro(install: bool) -> anyhow::Result<WslInit> {
     let wsl = get_wsl_lib()?;
     let meta_path = config_dir()?.join("wsl.json");
@@ -538,7 +541,7 @@ fn get_wsl_distro(install: bool) -> anyhow::Result<WslInit> {
             fs::create_dir_all(&download_dir)?;
 
             let download_path = download_dir.join("debian.zip");
-            download(&download_path, &DISTRO_URL, false)?;
+            download_sync(&download_path, &DISTRO_URL, false)?;
 
             msg!("Unpacking WSL distribution...");
             let file_format = detect_file_format(&download_path)?;

src/project/sync.rs
@@ -176,7 +176,7 @@ async fn sync(
     }
 
     msg!("3. Applying config...");
-    match project::config::apply(&project, true, skip_hooks).await {
+    match project::config::apply(project, true, skip_hooks).await {
         Ok(true) => {
             print::success!("Project is now in sync.")
         }

src/watch/mod.rs
@@ -361,7 +361,7 @@ async fn watch_and_match(
         // strip prefix
         let changed_paths: Vec<_> = changed_paths
             .iter()
-            .flat_map(|p| p.strip_prefix(&project_root).ok())
+            .flat_map(|p| p.strip_prefix(project_root).ok())
             .map(|p| (p, wax::CandidatePath::from(p)))
             .collect();
 

commit 5688cb5419b51bab5bada6454dbdc3e74d55fd4c

Fix `gel cli upgrade`
Broken by #1752.
Fixes #1753.

People who have upgraded to a broken version will probably need to do
a reinstall, though :(

src/cli/upgrade.rs
@@ -9,7 +9,7 @@ use indicatif::{ProgressBar, ProgressStyle};
 
 use crate::platform::{binary_path, current_exe, home_dir, tmp_file_path};
 use crate::portable::platform;
-use crate::portable::repository::{self, Channel, download};
+use crate::portable::repository::{self, Channel, download_sync};
 use crate::portable::ver;
 use crate::print::{self, Highlight, msg};
 use crate::process;
@@ -49,8 +49,7 @@ pub fn run(cmd: &Command) -> anyhow::Result<()> {
     upgrade(cmd, _get_upgrade_path()?)
 }
 
-#[tokio::main(flavor = "current_thread")]
-async fn upgrade(cmd: &Command, path: PathBuf) -> anyhow::Result<()> {
+fn upgrade(cmd: &Command, path: PathBuf) -> anyhow::Result<()> {
     let cur_channel = channel();
     let channel = if let Some(channel) = cmd.to_channel {
         channel
@@ -96,7 +95,7 @@ async fn upgrade(cmd: &Command, path: PathBuf) -> anyhow::Result<()> {
     let down_path = path.with_extension("download");
     let tmp_path = tmp_file_path(&path);
 
-    download(&down_path, &pkg.url, cmd.quiet).await?;
+    download_sync(&down_path, &pkg.url, cmd.quiet)?;
     unpack_file(&down_path, &tmp_path, pkg.compression)?;
 
     let backup_path = path.with_extension("backup");